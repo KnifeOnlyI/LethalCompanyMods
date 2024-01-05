a75283c87cac86e5d6b7224ea798d9bb9265b94f
KnifeOnlyI <dany.pignoux@outlook.fr>
2024-01-05 17:20:46 +0100

    Unlimited Stamina : Add new mod

---
+++ UnlimitedStamina/Mod.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Koi.LethalCompany.UnlimitedStamina
+{
+    /// <summary>
+    /// The mod entry point.
+    /// </summary>
+    [BepInPlugin(ModConstants.Meta.ModGuid, ModConstants.Meta.ModName, ModConstants.Meta.ModVersion)]
+    public class Mod : BaseUnityPlugin
+    {
+        /// <summary>
+        /// The mod instance.
+        /// </summary>
+        private static Mod _instance;
+
+        /// <summary>
+        /// The mod logger.
+        /// </summary>
+        private static ManualLogSource _modLogger;
+
+        /// <summary>
+        /// The harmony instance to patch game methods.
+        /// </summary>
+        private readonly Harmony _harmony = new Harmony(ModConstants.Meta.ModGuid);
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+
+            _modLogger = BepInEx.Logging.Logger.CreateLogSource(ModConstants.Meta.ModGuid);
+
+            _modLogger.LogInfo("The mod has been loaded successfully");
+
+            ModConfig.Init(Config);
+
+            _harmony.PatchAll();
+        }
+    }
+}
---
+++ UnlimitedStamina/Patches/PlayerControllerBPatches.cs
@@ -0,0 +1,27 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace Koi.LethalCompany.UnlimitedStamina.Patches
+{
+    /// <summary>
+    /// All patches for the PlayerControllerB class.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatches
+    {
+        /// <summary>
+        /// Disable the stamina consumption.
+        /// </summary>
+        /// <param name="__instance"></param>
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        // ReSharper disable once InconsistentNaming
+        private static void InfiniteStamina(PlayerControllerB __instance)
+        {
+            if (!ModConfig.ConfigEnabled.Value) return;
+
+            __instance.sprintMeter = 1f;
+            __instance.isExhausted = false;
+        }
+    }
+}
